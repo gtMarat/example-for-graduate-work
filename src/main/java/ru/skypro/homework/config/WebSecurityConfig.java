6ff9ff35374e07cd52f550bf19cbc473b0eee984
Arthur <guritch21@gmail.com>
2023-11-29 23:24:14 +0300

    corrections in authorization, DONE (spring 3.0.5)

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -3,27 +3,19 @@ package ru.skypro.homework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import static org.springframework.security.config.Customizer.withDefaults;
-@EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 
 public class WebSecurityConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
-//    }
     // переменная с адресами страниц, которые открываются без аутентификации.
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -57,7 +49,6 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
 
 
